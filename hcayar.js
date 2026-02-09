12041b14b02ede75442419235e3c238576e28746
Huseyn Mammadli <149963247+Silgimusicbot@users.noreply.github.com>
2026-02-09 20:58:16 +0400

    Prevent default context menu and key actions

--- hcayar.js
+++ hcayar.js
@@ -7,7 +7,6 @@ const config = {
     musicTitle: "Cəmaləm Üçün"
 };
 document.addEventListener('contextmenu', event => event.preventDefault());
-
 document.onkeydown = function(e) {
     if (e.keyCode == 123) return false; 
     if (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0))) return false;
